958ae20342581bebf395b32306f5c048db4442bb
靳兆鲁 <1756404649@qq.com>
2019-04-01 19:37:30 -0700

    添加MP4转GIF脚本
    该脚本基于FFMPEG请确保您的机器安装了FFMPEG

---
+++ shell-scripts/mp4-to-gif.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+LOCAL_PATH=$(cd `dirname $0`; pwd)
+cd $LOCAL_PATH
+
+WORK_PATH=$1
+
+echo  ">>进入目录" $WORK_PATH
+
+cd $WORK_PATH
+
+if [ ! -d "./pic/" ];then
+mkdir ./pic/
+#else
+#echo "文件夹已经存在"
+fi
+
+if [ ! -d "./gif/" ];then
+mkdir ./gif/
+#else
+#echo "文件夹已经存在"
+fi
+
+echo  ">>>搜索文件"
+
+for file in $WORK_PATH/* 
+do
+
+if [[ "${file##*.}" == "mp4" ]];
+then
+  FILE_NAME=${file##*/}
+  echo ">>>>发现MP4文件 "   "${file##*/}" "${FILE_NAME%%.*}"
+  echo -e "\033[31m为${file##*/}创建全局调色板 " " ${FILE_NAME%%.*}.png\033[0m"
+  ffmpeg  -i ${file} -b 568k -r 20 -vf fps=20,scale=320:-1:flags=lanczos,palettegen -y ./pic/"${FILE_NAME%%.*}.png" >/dev/null
+  echo -e "\033[31m为${file##*/}生成GIF " " ${FILE_NAME%%.*}.gif\033[0m"
+  filters="fps=15,scale=320:-1:flags=lanczos"
+  ffmpeg -v warning -i ${file} -i ./pic/${FILE_NAME%%.*}.png -r 15 -lavfi "$filters [x]; [x][1:v] paletteuse" -y ./gif/"${FILE_NAME%%.*}.gif" >/dev/null
+fi
+done
